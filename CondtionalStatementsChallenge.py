6090eb48efb5806201d6c8dc5e49144910f4bf5c
AnyMoment <48554017+AnyMoment@users.noreply.github.com>
2019-04-01 18:11:56 +0530

    Add files via upload

---
+++ CondtionalStatementsChallenge.py
@@ -0,0 +1,28 @@
+
+#Read 5 subjects, display failed status, otherwise compute average and display grade 
+import functools
+subjectMarks= [int(x) for x in input("Enter Subjects seperated by space:").split()]
+print("Subject marks entered are:",subjectMarks)
+
+temp=list(filter(lambda x: x>50,subjectMarks))
+if subjectMarks==temp:
+      averageSubjects= functools.reduce(lambda x,y: x+y,subjectMarks)/float (len(subjectMarks))
+      print("The average is",averageSubjects)
+      if averageSubjects > 80 :
+            print("grade A")
+      elif averageSubjects > 70 :
+            print("grade B")
+      elif averageSubjects > 50:
+            print("grade C")
+else:
+      print("Failed a subject, result not caluculated")          
+
+
+       
+       
+       
+       
+       
+
+
+     
